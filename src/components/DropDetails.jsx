5770a2ef3bb307d365f3b94e6c04c78b0696fae0
RizwanAmjad <rizwanamjadms@gmail.com>
2022-09-26 14:48:41 +0500

    Done styling the create drop UI

--- src/components/DropDetails.jsx
+++ src/components/DropDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useCallback } from "react"
 
+import styles from "./css/drop-details.module.css"
+
 function DropDetails({ dropInput, setDropInput, onCreateDrop }) {
   const handleMetadataSelected = useCallback(
     ({ target }) => {
@@ -16,38 +18,40 @@ function DropDetails({ dropInput, setDropInput, onCreateDrop }) {
   )
 
   return (
-    <div>
-      <div>
-        <label>Collection Name</label>
-        <input
-          placeholder="Name"
-          value={dropInput.name}
-          onChange={({ target }) =>
-            setDropInput({ ...dropInput, name: target.value })
-          }
-        />
-      </div>
-      <div>
-        <label>Symbol</label>
-        <input
-          placeholder="Your collection Symbol"
-          value={dropInput.symbol}
-          onChange={({ target }) =>
-            setDropInput({ ...dropInput, symbol: target.value })
-          }
-        />
-      </div>
+    <div className={styles.container}>
+      <div className={styles.border}>
+        <div className={styles.formElement}>
+          <label>Collection Name</label>
+          <input
+            placeholder="Name"
+            value={dropInput.name}
+            onChange={({ target }) =>
+              setDropInput({ ...dropInput, name: target.value })
+            }
+          />
+        </div>
+        <div className={styles.formElement}>
+          <label>Symbol</label>
+          <input
+            placeholder="Your collection Symbol"
+            value={dropInput.symbol}
+            onChange={({ target }) =>
+              setDropInput({ ...dropInput, symbol: target.value })
+            }
+          />
+        </div>
 
-      <div>
-        <label>Metadata</label>
-        <input
-          type="file"
-          accept="application/json"
-          placeholder="Json file for metadatas"
-          onChange={handleMetadataSelected}
-        />
+        <div className={styles.formElement}>
+          <label>Metadata</label>
+          <input
+            type="file"
+            accept="application/json"
+            placeholder="Json file for metadatas"
+            onChange={handleMetadataSelected}
+          />
+        </div>
       </div>
-      <div>
+      <div className={styles.buttonContainer}>
         <button onClick={onCreateDrop}>Create Drop</button>
       </div>
     </div>
